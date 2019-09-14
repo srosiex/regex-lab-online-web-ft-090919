275e35535b266e805c83b703b9b96f44e447f671
Sarah H <sarahrose.henderson@gmail.com>
2019-09-14 16:20:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words = text.split(" ")
+    words.grep(/^un\w+ing$/)
+  end
 end
 
 def words_five_letters_long(text)
