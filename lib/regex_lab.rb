ff120ca210963fa4f6616ec58b69ee54ba0ad32b
Sarah H <sarahrose.henderson@gmail.com>
2019-09-14 16:22:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 
 
 def words_five_letters_long(text)
-
+  words = text.split(" ")
+    words.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
